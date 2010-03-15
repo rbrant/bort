8bb9ae7a321aede6d04a88ea6c666a79e8f528c6
Richard Brant <rbrant@gmail.com>
2010-03-15 14:47:09 -0400

    removed prototype added jquery 1.4.2 added jrails plugin added paperclip added uuid tools added delayed_job (fork) removed asset packager plugin css changes added formatastic plugin nips/tucks for Rails 2.3.5

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.2.0' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -30,6 +30,12 @@ Rails::Initializer.run do |config|
   config.gem "capistrano-ext", :lib => "capistrano"
   config.gem 'rubyist-aasm', :lib => 'aasm', :source => 'http://gems.github.com', :version => '2.0.2'
   config.gem 'mislav-will_paginate', :version => '2.3.6', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem 'rack-openid', :lib => 'rack/openid'
+  config.gem 'ruby-openid', :lib => 'openid'
+  config.gem 'uuidtools'
+  config.gem 'paperclip'
+  # fork of delayed job that has better rake tasks/generators
+  config.gem 'collectiveidea-delayed_job', :lib => 'delayed_job', :source => 'http://gems.github.com'
   
   # These cause problems with irb. Left in for reference
   # config.gem 'rspec-rails', :lib => 'spec/rails', :version => '1.1.11'
