244eb6158673f382ff1244d4a4769e3d37586ba7
Richard Brant <rbrant@gmail.com>
2010-03-22 22:27:09 -0400

    missing route added

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ ActionController::Routing::Routes.draw do |map|
   map.change_password '/change_password/:reset_code', :controller => 'passwords', :action => 'reset'
   map.open_id_complete '/opensession', :controller => "sessions", :action => "create", :requirements => { :method => :get }
   map.open_id_create '/opencreate', :controller => "users", :action => "create", :requirements => { :method => :get }
-  
+  map.update_after_forgetting '/update_after_forgetting', :controller => 'passwords', :action => 'update_after_forgetting'
   # Restful Authentication Resources
   map.resources :users
   map.resources :passwords
